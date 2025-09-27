a7ab9311a60bf65fc0d1d89c117293b90db03a51
Matthew Gray <phlip45@gmail.com>
2025-09-27 00:08:21 -0700

    before 4.5

---
+++ DesignObsidian/granny-like/Cutscene/Closing.md
@@ -0,0 +1,11 @@
+Fade to black
+
+- I ran for as long as I could
+- The things I saw in that place...
+- I found a kindly driver who gave me a ride to my son's home
+<span style="color:rgb(255, 255, 0)">Daniel Comes in to the conversation</span> 
+Daniel: Ma what are you doing here! and you're filthy! 
+pause
+Daniel: Whoa whoa whoa calm down, you aren't making any sense. I'll call right now and get to the bottom of this
+pause -> Maybe pause is peanuts style mumbling?
+Daniel: 
--- test/door.gd
+++ test/door.gd
@@ -6,14 +6,18 @@ var open:bool = false
 @export var mirror_for_opening:bool = false
 
 func interact(_player:Player, _interact_area:Interactable):
-	if !unlocked: 
+	if _player.has_key(Global.Key_Type.GATE):
+		var anim_string = "open_door_mirror"  if mirror_for_opening else "open_door"
+		if open:
+			animation_player.play_backwards(anim_string)
+		else:
+			animation_player.play(anim_string)
+		var tween = create_tween()
+		tween.tween_property(_player.blinders,"color",Color(0.0, 0.0, 0.0),2)
+		tween.tween_callback(roll_credits)
+	elif !unlocked: 
 		Global.event_bus.emit(Global.BusType.PLAYER_NOTIFICATION, "Door requires gate key")
 		return
 		
-	var anim_string = "open_door_mirror"  if mirror_for_opening else "open_door"
-	if open:
-		animation_player.play_backwards(anim_string)
-	else:
-		animation_player.play(anim_string)
-	open = !open
-		
+func roll_credits():
+	pass
